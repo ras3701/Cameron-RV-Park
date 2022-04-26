2c52136420af2cc314b69c6cd8483d0c40bbede3
Rohan Shah <84697720+ras3701@users.noreply.github.com>
2022-04-26 04:55:56 -0500

    Fixed Start Date and End Date filter for All Bookings. Also, cleaned up all the filters created so far.

--- adminhome/filters.py
+++ adminhome/filters.py
@@ -1,50 +1,70 @@
+from dataclasses import field
+from django.forms import DateInput, widgets
 import django_filters
 from .models import  ParkingCategory, ParkingSpot, Booking, Vehicle
 from .forms import DatePickerInput
 
 class ParkingCatergoryFilter(django_filters.FilterSet):
-    # name = django_filters.CharFilter(lookup_expr='icontains')
+    ps_status = django_filters.BooleanFilter(label='Is Parking Category Active', field_name='is_active')
 
     class Meta:
         model = ParkingCategory
-        fields = ('is_active',)
+        fields = ('ps_status',)
 
 class ParkingSpotFilter(django_filters.FilterSet):
+    ps_status = django_filters.BooleanFilter(label='Is Parking Spot Active', field_name='is_active')
+    parking_cat_name = django_filters.AllValuesFilter(label='Parking Category Name', field_name='parking_category_id__name')
 
     class Meta:
         model = ParkingSpot
-        fields = ('is_active', 'parking_category_id',)
+        fields = ('ps_status', 'parking_cat_name',)
 
 class BookingFilter(django_filters.FilterSet):
+    parking_spot_name = django_filters.AllValuesFilter(label='Parking Spot Name', field_name='parking_spot_id__name')
+    is_lease_signed_by_user = django_filters.BooleanFilter(label='Is Lease Signed by the User', field_name='lease_is_signed_by_user')
+    booking_state = django_filters.AllValuesFilter(label='Booking State', field_name='state')
+    username = django_filters.CharFilter(label='Username', field_name='vehicle_id__user_id__username')
 
     class Meta:
         model = Booking
-        fields = ('parking_spot_id', 'lease_is_signed_by_user', 'state', 'vehicle_id__user_id',)
-        # fields = {
-        #     'parking_spot_id': ['exact'], 
-        #     'start_time': ['gte'], 
-        #     'end_time': ['lte'], 
-        #     'lease_is_signed_by_user': ['exact'], 
-        #     'state': ['exact'],
-        # }
-        # widgets = {
-        #     'start_time': DatePickerInput,
-        # }
+        fields = ('parking_spot_name', 'is_lease_signed_by_user', 'booking_state', 'username',)
 
 class PreviousAndCurrentBookingFilter(django_filters.FilterSet):
+    parking_spot_name = django_filters.AllValuesFilter(label='Parking Spot Name', field_name='parking_spot_id__name')
+    booking_state = django_filters.AllValuesFilter(label='Booking State', field_name='state')
+    username = django_filters.CharFilter(label='Username', field_name='vehicle_id__user_id__username')
 
     class Meta:
         model = Booking
-        fields = ('parking_spot_id', 'state', 'vehicle_id__user_id',)
+        fields = ('parking_spot_name', 'booking_state', 'username',)
 
 class AllBookingFilter(django_filters.FilterSet):
+    parking_cat_name = django_filters.AllValuesFilter(label='Parking Category Name', field_name='pc_id__name')
+    parking_spot_name = django_filters.AllValuesFilter(label='Parking Spot Name', field_name='parking_spot_id__name')
+    is_lease_signed_by_user = django_filters.BooleanFilter(label='Is Lease Signed by the User', field_name='lease_is_signed_by_user')
+    booking_state = django_filters.AllValuesFilter(label='Booking State', field_name='state')
+    username = django_filters.CharFilter(label='Username', field_name='vehicle_id__user_id__username')
+    vehicle_name = django_filters.CharFilter(label='Vehicle Name', field_name='vehicle_id__name')
+
+    # start_time = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}), lookup_expr='start_time__gte=value')
+    # end_time = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}))
+    start_date = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}), method='filter_start_date', label='Start Date')
+    end_date = django_filters.DateFilter(widget=DateInput(attrs={'type': 'date'}), method='filter_end_date', label='End Date')
+
+    def filter_start_date(self, queryset, name, value):
+        return queryset.filter(start_time__gte=value)
+
+    def filter_end_date(self, queryset, name, value):
+        return queryset.filter(end_time__lte=value)
 
     class Meta:
         model = Booking
-        fields = ('pc_id', 'parking_spot_id', 'lease_is_signed_by_user', 'start_time', 'end_time', 'state', 'vehicle_id__user_id', 'vehicle_id',)
+        fields = ('parking_cat_name', 'parking_spot_name', 'is_lease_signed_by_user', 'booking_state', 'username', 'vehicle_name', 'start_date', 'end_date',)
+
 
 class UnverifiedVehiclesFilter(django_filters.FilterSet):
+    username = django_filters.CharFilter(label='Username', field_name='user_id__username')
 
     class Meta:
         model = Vehicle
-        fields = ('user_id',)
+        fields = ('username',)
